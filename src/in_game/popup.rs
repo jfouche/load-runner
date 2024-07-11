f575719cbaa693c00c1b0a4c2cd49bd64dae4dda
Jérémie Fouché <jeremie.fouche@gmail.com>
2024-07-11 12:57:48 +0200

    better looking popup

--- src/in_game/popup.rs
+++ src/in_game/popup.rs
@@ -1,7 +1,6 @@
 use super::{GameState, InGameState, Temporary};
 use crate::ui::*;
 use bevy::prelude::*;
-use std::time::Duration;
 
 pub fn popup_plugin(app: &mut App) {
     app.add_systems(
@@ -32,7 +31,7 @@ pub enum PopupImage {
 
 #[derive(Component, Default)]
 enum PopupCloseEvent {
-    Duration(Duration),
+    // Duration(Duration),
     #[default]
     KeyPressed,
 }
@@ -50,7 +49,7 @@ impl Default for PopupBundle {
         PopupBundle {
             content: PopupContent::default(),
             name: Name::new("Popup"),
-            node: centered(),
+            node: popup(),
             close_event: PopupCloseEvent::default(),
         }
     }
@@ -64,14 +63,14 @@ impl PopupBundle {
         }
     }
 
-    pub fn auto_despawn(self) -> Self {
-        PopupBundle {
-            content: self.content,
-            name: self.name,
-            node: self.node,
-            close_event: PopupCloseEvent::Duration(Duration::from_secs_f32(2.0)),
-        }
-    }
+    // pub fn auto_despawn(self) -> Self {
+    //     PopupBundle {
+    //         content: self.content,
+    //         name: self.name,
+    //         node: self.node,
+    //         close_event: PopupCloseEvent::Duration(Duration::from_secs_f32(2.0)),
+    //     }
+    // }
 }
 
 fn spawn_popup(
@@ -79,37 +78,19 @@ fn spawn_popup(
     popups: Query<(Entity, &PopupContent, &PopupCloseEvent), Added<PopupContent>>,
     mut in_game_state: ResMut<NextState<InGameState>>,
 ) {
-    for (entity, content, close_event) in &popups {
-        commands.entity(entity).with_children(|screen| {
-            screen.spawn(menu()).with_children(|menu| {
-                menu.spawn(menu_title(&content.title));
-                menu.spawn(TextBundle::from_section(
-                    content.text.clone(),
-                    TextStyle {
-                        font_size: 24.0,
-                        color: Color::WHITE,
-                        ..Default::default()
-                    },
-                ));
-                for image in &content.images {
-                    match image {
-                        PopupImage::AtlasImage {
-                            texture_atlas,
-                            image,
-                        } => {
-                            menu.spawn(AtlasImageBundle {
-                                texture_atlas: texture_atlas.clone(),
-                                image: image.clone(),
-                                ..Default::default()
-                            });
-                        }
-                    }
-                }
+    for (entity, content, _close_event) in &popups {
+        commands.entity(entity).with_children(|menu| {
+            menu.spawn(popup_title_bar()).with_children(|title_bar| {
+                title_bar.spawn(popup_title(&content.title));
             });
+            menu.spawn(popup_text_content(&content.text));
+            for image in &content.images {
+                menu.spawn(popup_image(image));
+            }
         });
-        if let PopupCloseEvent::Duration(duration) = close_event {
-            commands.entity(entity).insert(Temporary::new(*duration));
-        }
+        // if let PopupCloseEvent::Duration(duration) = close_event {
+        //     commands.entity(entity).insert(Temporary::new(*duration));
+        // }
         in_game_state.set(InGameState::ShowPopup);
     }
 }
@@ -125,5 +106,78 @@ fn close_popup(
             commands.entity(entity).despawn_recursive();
             in_game_state.set(InGameState::Running);
         }
+    }
+}
+
+fn popup() -> NodeBundle {
+    let vsizer = vsizer();
+    NodeBundle {
+        background_color: Color::rgb(0.25, 0.25, 0.25).into(),
+        border_color: Color::BLACK.into(),
+        style: Style {
+            border: UiRect::all(Val::Px(2.0)),
+            width: Val::Percent(35.0),
+            margin: UiRect::all(Val::Auto),
+            padding: UiRect {
+                left: Val::Px(0.0),
+                right: Val::Px(0.0),
+                top: Val::Px(0.0),
+                bottom: Val::Px(7.0),
+            },
+            ..vsizer.style
+        },
+        ..vsizer
+    }
+}
+
+fn popup_title_bar() -> NodeBundle {
+    NodeBundle {
+        background_color: Color::rgb(0.1, 0.1, 0.1).into(),
+        style: Style {
+            width: Val::Percent(100.0),
+            justify_content: JustifyContent::Center,
+            padding: UiRect::all(Val::Px(2.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    }
+}
+
+fn popup_title(title: &str) -> TextBundle {
+    TextBundle::from_section(
+        title,
+        TextStyle {
+            font_size: 32.0,
+            color: Color::rgb(0.72, 0.72, 0.72),
+            ..default()
+        },
+    )
+}
+
+fn popup_text_content(content: &str) -> TextBundle {
+    TextBundle::from_section(
+        content,
+        TextStyle {
+            font_size: 24.0,
+            color: Color::WHITE,
+            ..Default::default()
+        },
+    )
+    .with_style(Style {
+        margin: UiRect::all(Val::Px(7.0)),
+        ..Default::default()
+    })
+}
+
+fn popup_image(image: &PopupImage) -> impl Bundle {
+    match image {
+        PopupImage::AtlasImage {
+            texture_atlas,
+            image,
+        } => AtlasImageBundle {
+            texture_atlas: texture_atlas.clone(),
+            image: image.clone(),
+            ..Default::default()
+        },
     }
 }
